5d9ad53c4bfd022137435b83d5db3d0efb513cc9
Cecilia <fili.cecilia@gmail.com>
2018-10-16 17:02:14 +0200

    comunication betwen Raspberry pi and humidity and temperature sensor

--- sensor_tempera_humedad/sensor_tempera_humedad.ino
+++ sensor_tempera_humedad/sensor_tempera_humedad.ino
@@ -1,20 +1,37 @@
+
+//Humidity sensor: where is the 's' = DO, middle = V++, the another one = GNN
+
 #include <dht.h>
-//donde esta a s es la salida, medio voltaje, otro lado masa
+#include <Wire.h>
 
 dht DHT;
-
+int temperature;
+int humidity;
 #define DHT11_PIN 7
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write(temperature); // respond with message of 6 bytes as expected by master
+  Wire.write(humidity);
+  
+}
+
 
 void setup(){
   Serial.begin(9600);
+   Wire.begin(8);                // join i2c bus with address #8
+   Wire.onRequest(requestEvent); // register event
 }
 
 void loop()
 {
   int chk = DHT.read11(DHT11_PIN);
   Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
+  Serial.println(DHT.temperature); 
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
+  temperature = DHT.temperature*100;
+  humidity = DHT.humidity*100;
   delay(1000);
 }
